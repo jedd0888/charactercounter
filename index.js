6a6e9cd03af90da3213d968c8d42f6229119b829
xgoje <jedd@moogah.net>
2022-02-08 08:06:41 +1100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {
     green
 } = require("colorette");
 
-const readline = require('readline').createInterface({
+const readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
 });
